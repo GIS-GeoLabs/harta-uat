b417774f05e88df27ca2f14056aefb11f52f8790
valentingheorghev <valentin.gheorghev@gmail.com>
2026-02-26 20:44:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const map = L.map('map').setView([45.9, 24.9], 7);
 
 // 2. Variabilă pentru layer
 let layerJudete;
+let layerUAT;
 
 // 3. Încărcăm GeoJSON-ul cu județe
 fetch('judete.geojson')
@@ -11,19 +12,55 @@ fetch('judete.geojson')
 
     layerJudete = L.geoJSON(data, {
       style: {
-        color: '#444',
+        color: '#000',
         weight: 1,
-        fillColor: '#9ecae1',
+        fillColor: '#4da3ff',
         fillOpacity: 0.7
       },
       onEachFeature: (feature, layer) => {
-
-        // La click pe județ (DOAR afișăm numele)
         layer.on('click', () => {
-          alert(feature.properties.Judet);
+          afiseazaUAT(feature.properties.Judet);
         });
-
       }
     }).addTo(map);
 
   });
+
+function afiseazaUAT(judetSelectat) {
+
+  // scoatem județele
+  if (layerJudete) {
+    map.removeLayer(layerJudete);
+  }
+
+  // scoatem UAT-urile vechi (dacă există)
+  if (layerUAT) {
+    map.removeLayer(layerUAT);
+  }
+
+  // încărcăm UAT-urile
+  fetch('uat.geojson')
+    .then(r => r.json())
+    .then(data => {
+
+      layerUAT = L.geoJSON(data, {
+        filter: f => f.properties.Judet === judetSelectat,
+        style: {
+          color: '#333',
+          weight: 0.8,
+          fillColor: '#ffcc66',
+          fillOpacity: 0.7
+        },
+        onEachFeature: (feature, layer) => {
+
+          // CLICK pe UAT → redirect la URL
+          layer.on('click', () => {
+            window.location.href = feature.properties.URL;
+          });
+
+        }
+      }).addTo(map);
+
+    });
+}
+
