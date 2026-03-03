d3decabb7e2c9711cbd41edb23bcd4027520a4c0
GISteam <valentin.gheorghev@gmail.com>
2026-03-03 10:08:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ function norm(txt) {
   return result;
 }
 
+// ----- HASH STABIL PENTRU JUDEȚ -----
 function stringToHash(str) {
   var hash = 0;
   for (var i = 0; i < str.length; i++) {
@@ -26,6 +27,7 @@ function getBlueFromName(name) {
   return 'hsl(' + hue + ',' + saturation + '%,' + lightness + '%)';
 }
 
+// ----- FORMAT LABEL UAT -----
 function formatUATName(name) {
   if (!name) return '';
   if (name.length <= 10) return name;
@@ -40,12 +42,81 @@ function formatUATName(name) {
   return name.substring(0, splitAt) + '<br>' + name.substring(splitAt + 1);
 }
 
+// ----- CENTROID LOGIC (NU SE MODIFICĂ) -----
+function getLargestPolygonRings(coords) {
+  var best = null, maxArea = -1;
+  for (var i = 0; i < coords.length; i++) {
+    var ring = coords[i][0], area = 0;
+    for (var j = 0; j < ring.length - 1; j++) {
+      area += ring[j][0] * ring[j + 1][1];
+      area -= ring[j + 1][0] * ring[j][1];
+    }
+    area = Math.abs(area / 2);
+    if (area > maxArea) { maxArea = area; best = coords[i]; }
+  }
+  return best;
+}
+
+function ringCentroid(ring) {
+  var x = 0, y = 0, area = 0;
+  var n = ring.length - 1;
+  for (var i = 0, j = n - 1; i < n; j = i++) {
+    var xi = ring[i][0], yi = ring[i][1];
+    var xj = ring[j][0], yj = ring[j][1];
+    var f = xi * yj - xj * yi;
+    area += f;
+    x += (xi + xj) * f;
+    y += (yi + yj) * f;
+  }
+  area /= 2;
+  if (Math.abs(area) < 1e-12) {
+    var cx = 0, cy = 0;
+    for (var k = 0; k < n; k++) { cx += ring[k][0]; cy += ring[k][1]; }
+    return [cx / n, cy / n];
+  }
+  return [x / (6 * area), y / (6 * area)];
+}
+
+function pointInRing(pt, ring) {
+  var x = pt[0], y = pt[1], inside = false;
+  for (var i = 0, j = ring.length - 1; i < ring.length; j = i++) {
+    var xi = ring[i][0], yi = ring[i][1];
+    var xj = ring[j][0], yj = ring[j][1];
+    if (((yi > y) !== (yj > y)) && (x < (xj - xi) * (y - yi) / (yj - yi) + xi)) {
+      inside = !inside;
+    }
+  }
+  return inside;
+}
+
+function getLabelLatLng(feature, layer) {
+  if (feature.properties.LabelLat && feature.properties.LabelLng) {
+    return L.latLng(feature.properties.LabelLat, feature.properties.LabelLng);
+  }
+  var rings = null;
+  if (feature.geometry.type === 'Polygon') {
+    rings = feature.geometry.coordinates;
+  } else if (feature.geometry.type === 'MultiPolygon') {
+    rings = getLargestPolygonRings(feature.geometry.coordinates);
+  }
+  if (!rings) return layer.getBounds().getCenter();
+  var ring = rings[0];
+  var c = ringCentroid(ring);
+  if (pointInRing(c, ring)) return L.latLng(c[1], c[0]);
+  var cx = 0, cy = 0, n = ring.length - 1;
+  for (var k = 0; k < n; k++) { cx += ring[k][0]; cy += ring[k][1]; }
+  return L.latLng(cy / n, cx / n);
+}
+
 // ================== MAP ==================
 var map = L.map('map').setView([45.9, 24.9], 7);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; OpenStreetMap',
-  maxZoom: 19
+  maxZoom: 19,
+  updateWhenIdle: true,
+  updateWhenZooming: false,
+  keepBuffer: 2
 }).addTo(map);
 
 var MIN_UAT_LABEL_ZOOM = 10;
@@ -68,7 +139,7 @@ var backBtn = document.getElementById('backBtn');
 backBtn.onclick = function() {
   uatActive = false;
   if (layerUAT) map.removeLayer(layerUAT);
-  uatLabels.forEach(function(l) { map.removeLayer(l); });
+  for (var i = 0; i < uatLabels.length; i++) map.removeLayer(uatLabels[i]);
   uatLabels = [];
   if (layerJudete) layerJudete.addTo(map);
   map.setView([45.9, 24.9], 7);
@@ -130,12 +201,11 @@ fetch('judete.geojson')
 
 // ================== UAT ==================
 function afiseazaUAT(judetSelectat) {
-
   uatActive = true;
 
   if (layerJudete) map.removeLayer(layerJudete);
   if (layerUAT) map.removeLayer(layerUAT);
-  uatLabels.forEach(function(l) { map.removeLayer(l); });
+  for (var i = 0; i < uatLabels.length; i++) map.removeLayer(uatLabels[i]);
   uatLabels = [];
 
   fetch('uat.geojson')
@@ -146,46 +216,49 @@ function afiseazaUAT(judetSelectat) {
 
       layerUAT = L.geoJSON(data, {
         renderer: canvasRenderer,
-
         filter: function(f) {
           return norm(f.properties.Judet) === norm(judetSelectat);
         },
-
-        style: {
-          color: '#000',
-          weight: 1.5,
-          fillColor: '#ffe599',
-          fillOpacity: 0.9
-        },
+        style: { color: '#000', weight: 1.5, fillColor: '#ffe599', fillOpacity: 0.9 },
 
         onEachFeature: function(feature, layer) {
 
+          var labelLatLng = getLabelLatLng(feature, layer);
+
+          var label = L.marker(labelLatLng, {
+            icon: L.divIcon({
+              className: 'uat-label-wrap',
+              html: '<div class="label-uat">' + formatUATName(feature.properties.UAT) + '</div>',
+              iconSize: [0, 0],
+              iconAnchor: [0, 0]
+            }),
+            interactive: false,
+            keyboard: false
+          }).addTo(map);
+
+          uatLabels.push(label);
+
           layer.on('mouseover', function() {
-            layer.setStyle({
-              fillColor: '#f1c232',
-              weight: 3
-            });
+            layer.setStyle({ fillColor: '#f1c232', weight: 3 });
             layer.bringToFront();
+            var el = label.getElement();
+            if (el) el.querySelector('.label-uat').classList.add('label-hover');
           });
 
           layer.on('mouseout', function() {
-            layer.setStyle({
-              fillColor: '#ffe599',
-              weight: 1.5
-            });
+            layer.setStyle({ fillColor: '#ffe599', weight: 1.5 });
+            var el = label.getElement();
+            if (el) el.querySelector('.label-uat').classList.remove('label-hover');
           });
 
           layer.on('click', function() {
             if (feature.properties.URL)
               window.open(feature.properties.URL, '_blank');
           });
-
         }
-
       }).addTo(map);
 
       backBtn.style.display = 'block';
       map.getContainer().classList.remove('labels-hidden');
-
     });
 }
